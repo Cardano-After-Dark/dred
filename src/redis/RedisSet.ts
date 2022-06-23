e526e9737de985cba521fbf2069331a118323121
oldcryptogeek <oldcryptogeek@kodexdata.systems>
2022-06-23 14:52:15 -0600

    Update RedisSet.ts

--- src/redis/RedisSet.ts
+++ src/redis/RedisSet.ts
@@ -3,27 +3,27 @@ import Redis from "ioredis";
 type keyType = "_abstract" | string;
 
 export class RedisSet {
-  redis: Redis.Redis;
-  key: keyType;
-  abstract: boolean;
-  constructor(redis: Redis.Redis, key: keyType = "_abstract") {
-    this.redis = redis;
-    this.key = key;
-    this.abstract = !!(key === "_abstract");
-  }
-  async has(key: string, setName?: string) {
-    if (this.abstract && !setName)
-      throw new Error(`abstract RedisSet requires setName in arg2`);
-    return this.redis.send_command("SISMEMBER", setName || this.key, key);
-  }
-  async add(key: string, setName?: string) {
-    if (this.abstract && !setName)
-      throw new Error(`abstract RedisSet requires setName in arg2`);
-    return this.redis.send_command("SADD", this.key, key);
-  }
-  async delete(key: string, setName?: string) {
-    if (this.abstract && !setName)
-      throw new Error(`abstract RedisSet requires setName in arg2`);
-    return this.redis.send_command("SREM", this.key, key);
-  }
+    redis: Redis;
+    key: keyType;
+    abstract: boolean;
+    constructor(redis: Redis, key: keyType = "_abstract") {
+        this.redis = redis;
+        this.key = key;
+        this.abstract = !!(key === "_abstract");
+    }
+    async has(key: string, setName?: string) {
+        if (this.abstract && !setName)
+            throw new Error(`abstract RedisSet requires setName in arg2`);
+        return this.redis.call("SISMEMBER", setName || this.key, key);
+    }
+    async add(key: string, setName?: string) {
+        if (this.abstract && !setName)
+            throw new Error(`abstract RedisSet requires setName in arg2`);
+        return this.redis.call("SADD", this.key, key);
+    }
+    async delete(key: string, setName?: string) {
+        if (this.abstract && !setName)
+            throw new Error(`abstract RedisSet requires setName in arg2`);
+        return this.redis.call("SREM", this.key, key);
+    }
 }
