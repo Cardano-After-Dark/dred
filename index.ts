c2d902e27c9ef20a3b1c4104899d65d54affb55a
oldcryptogeek <oldcryptogeek@kodexdata.systems>
2022-06-23 14:52:52 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-require('module-alias/register')
+import "module-alias/register";
 import { createServer } from "./src/server";
 
-const concurrency = 50;
-const pendingTasks = new Set<Promise<any>>();
+// const concurrency = 50;
+// const pendingTasks = new Set<Promise<any>>();
 
 init();
 async function init() {
-    const server = await createServer();
-    const port = process.env.PORT || 3000
+    const port = process.env.PORT || 3000;
+    const server = await createServer({ port: port });
     server.listen(port);
 
     console.log(`Dred server listening on port ${port}`);
