0ed9dd64ed1e718d9aa9c21568c88c071fb1c4f7
Randall <randall@inator.biz>
2023-03-02 09:38:23 -0800

    Updated (possibly working ;) interface to Redis CuckooSet
     * will be useful for fast-no's on "has-message-id" questions.

--- src/redis/RedisCuckooSet.ts
+++ src/redis/RedisCuckooSet.ts
@@ -1,23 +1,35 @@
 import Redis from "ioredis";
 
-type CuckooSettings {
-  
+type CuckooSettings = {
+    initialCapacity: number;
 }
 
 export class RedisCuckooSet {
   redis: Redis.Redis;
   key: string;
+  settings: CuckooSettings;
   constructor(redis, key, settings: CuckooSettings) {
     this.redis = redis;
     this.key = key;
+    this.settings = settings
+    this.redis.call("CF.RESERVE", this.key, settings.initialCapacity)
   }
   async has(key: string) {
-    return this.redis.send_command("SISMEMBER", this.key, key);
+    return this.redis.call("CF.EXISTS", this.key, key);
   }
   async add(key: string) {
-    return this.redis.send_command("SADD", this.key, key);
+    return this.redis.call("CF.ADD", this.key, key);
+  }
+  async addIfNeeded(key: string) {
+    return this.redis.call("CF.ADDNX", this.key, key);
+  }
+  async count(key:string) {
+    return this.redis.call("CF.COUNT", this.key, key);
   }
   async delete(key: string) {
-    return this.redis.send_command("SREM", this.key, key);
+    return this.redis.call("CF.DELETE", this.key, key);
+  }
+  async info(key: string) {
+    return this.redis.call("CF.INFO", this.key, key);
   }
 }
