08dedffb61ba836764d55c8c57f71e2c740515b9
Randall <randall@inator.biz>
2025-03-21 11:05:46 -0700

    checpoint: docs: adding providers to page layout, remove search

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,4 +1,9 @@
 import withMarkdoc from "@markdoc/next.js"
+import path from "path"
+import { fileURLToPath } from "url"
+
+// Get the directory path equivalent to __dirname in CommonJS
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -10,7 +15,7 @@ const nextConfig = {
   basePath: "/dred",
   assetPrefix: "/dred/",
   // Add output configuration for static export
-  output: 'export',
+  output: "export",
   // Disable server-side features
   images: {
     unoptimized: true,
@@ -20,6 +25,43 @@ const nextConfig = {
   swcMinify: true,
   webpack: (config, { isServer }) => {
     config.output.publicPath = "/dred" + config.output.publicPath
+
+    config.resolve.modules = [
+        path.resolve(__dirname, "node_modules"),
+        path.resolve(__dirname, "../../stellar-contracts/node_modules")
+    ]
+
+    // config.resolve.alias["@donecollectively/stellar-contracts"] =
+    //     path.resolve(
+    //       __dirname,
+    //       "node_modules/@donecollectively/stellar-contracts"
+    //     )
+    //   config.resolve.alias["@donecollectively/stellar-contracts/ui"] =
+    //     path.resolve(
+    //       __dirname,
+    //       "node_modules/@donecollectively/stellar-contracts/ui.mjs"
+    //     )
+    //   config.resolve.alias["react"] = path.resolve(
+    //     __dirname,
+    //     "node_modules/react"
+    //   )
+    //   config.resolve.alias["react-dom"] = path.resolve(
+    //     __dirname,
+    //     "node_modules/react-dom"
+    //   )
+    //   config.resolve.alias["@helios-lang/compiler-utils"] = path.resolve(
+    //     __dirname,
+    //     "node_modules/@helios-lang/compiler-utils"
+    //   )
+      config.resolve.alias["@helios-lang/tx-utils"] = path.resolve(
+        __dirname,
+        "node_modules/@helios-lang/tx-utils"
+      )
+    //   config.resolve.alias["@helios-lang/ledger"] = path.resolve(
+    //     __dirname,
+    //     "node_modules/@helios-lang/ledger"
+    //   )
+
     // Add TypeScript support
     config.resolve.extensionAlias = {
       ".js": [".js", ".ts", ".tsx"],
--- docs/src/components/Providers.tsx
+++ docs/src/components/Providers.tsx
@@ -1,4 +1,4 @@
-import { DredCapoProvider } from "@/components/DredCapoProvider.jsx"
+import { DredCapoProvider } from "./DredCapoProvider.tsx"
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
