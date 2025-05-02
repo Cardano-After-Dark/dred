234768a74410df3b31a1dbb84a1021a722234822
psuzzi <psuzzi@gmail.com>
2025-05-03 00:56:54 +0200

    Add more errors to test build failure

--- src/broken-file.ts
+++ src/broken-file.ts
@@ -1,5 +1,6 @@
 // This file is deliberately broken to test the build failure notification
 import { ThisModuleDoesNotExist } from 'non-existent-module';
+import { AnotherFakeModule } from './this-file-does-not-exist';
 
 // Syntax error - missing semicolon and unclosed function
 function breakBuild() {
@@ -8,5 +9,13 @@ function breakBuild() {
     return x.nonExistentMethod()
 
 // Missing closing brace - will cause build to fail
+
+// More deliberate errors
+const anotherBrokenFunction = () => {
+  const y = "string"
+  y = 42 // Type error - reassigning a string to a number
+  
+  return y.split(1) // Another error - split takes a string
+}
 
 export const brokenExport = new ThisClassDoesNotExist(); 
