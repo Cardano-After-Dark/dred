b83042c885c9010ea36de5d7d4c71d6f9f1f8e1b
Randall <randall@inator.biz>
2025-06-06 08:47:20 -0700

    test: use server.pendingSetup() to await pending setup
    test log was reflecting duplicated create-default-channels activity.
    Code review showed that default-channel-creation was proceeding asynchronously,
    and that pendingSetup() is provided as a way to wait for it.  Use it.

--- src/server/testServer.ts
+++ src/server/testServer.ts
@@ -88,7 +88,7 @@ beforeEach(async () => {
         await server.redis?.flushdb();
         await server.reset();
         // testLogger.debug("beforeEach: establishing default channels");
-        await server.ensureDefaultChannels();
+        await server.pendingSetup();
         // server.listen();
     }
     testLogger.info("  ---- did reset redis with default channels in beforeEach");
