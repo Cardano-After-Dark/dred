f0eb2a7ede2715abf67aebdf11636972283c5c99
psuzzi <psuzzi@gmail.com>
2025-08-16 12:56:57 +0200

    change env variables read from node

--- src/peers/NeighborhoodDiscovery.ts
+++ src/peers/NeighborhoodDiscovery.ts
@@ -47,9 +47,11 @@ export class NeighborhoodDiscovery extends Discovery {
     }
 
     async initHostDiscovery() {
-        const network = environment.CARDANO_NETWORK;
+
+        // See also: environment.CARDANO_NETWORK;
+        const network = process.env.CARDANO_NETWORK;
         // console.log(process.env)
-        const bfKey = environment.BF_API_KEY 
+        const bfKey = process.env.BF_API_KEY 
         if (!bfKey) throw new Error("required env variable BF_API_KEY is not set");
         if (!network) throw new Error("required env variable CARDANO_NETWORK is not set");
 
