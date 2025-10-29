c3faee8ca94392b41e07e94b0e88f56755aaf4c0
Randall <randall@inator.biz>
2025-10-29 15:19:30 -0700

    docs: light code formatting

--- docs/src/components/Providers.tsx
+++ docs/src/components/Providers.tsx
@@ -1,10 +1,9 @@
-import { DredCapoProvider } from "dred-network-registry"
+import { DredCapoProvider } from "dred-network-registry";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (<>
-    <DredCapoProvider>
-    {children}
-    </DredCapoProvider>
-  </>
-  )
-}
+  return (
+    <>
+      <DredCapoProvider>{children}</DredCapoProvider>
+    </>
+  );
+};
